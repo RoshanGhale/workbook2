4d8869898ab50386e1f0083de3db47eaac2b6663
roshanghale <rghale0110@gmail.com>
2024-04-29 10:23:08 -0400

    add label

--- demos/1/add.js
+++ demos/1/add.js
@@ -1,4 +1,13 @@
+"use strict";
 var numberOne = 120;
 var numberTwo = 240;
 var result = numberOne + numberTwo;
 console.log(result);
+
+function add() {
+  var numberOne = 120;
+  var numberTwo = 240;
+  var result = numberOne + numberTwo;
+  console.log(result);
+}
+add();
